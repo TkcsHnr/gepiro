c3766d0fbe5ffed47c4f7839f01201ab8f67db22
Takacs Hunor <tkcshnr@gmail.com>
2023-12-03 17:14:41 +0100

    theme selector

---
+++ src/lib/components/ThemeSelector.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { themes } from '$lib/themes.json';
+    import { onMount } from 'svelte';
+    import { themeChange } from 'theme-change';
+
+	let darkMode = false;
+
+	// NOTE: the element that is using one of the theme attributes must be in the DOM on mount
+	onMount(() => {
+		themeChange(false);
+		// 👆 false parameter is required for svelte
+	});
+</script>
+
+<div class="flex w-full flex-wrap gap-2">
+	{#each themes as theme}
+        <button data-set-theme="{theme}" class="btn btn-lg font-mono flex flex-col border-base-300" data-theme={theme}>
+            {theme}
+            <p>
+                <span class="badge badge-primary">A</span>
+                <span class="badge badge-secondary">A</span>
+                <span class="badge badge-accent">A</span>
+            </p>
+        </button>
+	{/each}
+</div>
